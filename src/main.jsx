c526109a2dc08ce202dc304f16c16404d62ef857
clausanmartini-spinlab <clausanmartini@gmail.com>
2026-04-12 22:01:58 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Dashboard from './Dashboard.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const path = window.location.pathname
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {path === '/dashboard' ? <Dashboard /> : <App />}
+  </React.StrictMode>
 )
