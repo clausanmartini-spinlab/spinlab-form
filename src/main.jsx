37ee6033ac94ab45c4626815038db24c60245586
clausanmartini-spinlab <clausanmartini@gmail.com>
2026-05-29 16:31:57 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Dashboard from './Dashboard.jsx'
+import Upload from './Upload.jsx'
 
 const path = window.location.pathname
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {path === '/dashboard' ? <Dashboard /> : <App />}
+    {path === '/dashboard' ? <Dashboard /> :
+     path === '/upload' ? <Upload /> :
+     <App />}
   </React.StrictMode>
 )
